644f51a8df8b8490f4cf6c092e32ca64bc29c514
nattan447 <nattanferreira58@gmail.com>
2023-11-18 16:24:13 -0300

    all done

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const btn=document.getElementById("btn");
-let counter=0;
-class Tarefa  {
-    constructor(nome,id){
-        this.nome=nome;
-        this.id=id
-    } 
+const btn = document.getElementById("btn");
+let counter = 0;
+let counterinput = 10;
 
-editar()
-{
+class Tarefa {
+  constructor(nome, id, id_input) {
+    this.nome = nome;
+    this.id = id;
+    this.id_input = id_input;
+  }
 
+  editar() {
+    document.getElementById(this.id_input).disabled = false;
+    document
+      .getElementById(this.id_input)
+      .addEventListener("mouseleave", () => {
+        document.getElementById(this.id_input).disabled = true;
+      });
+  }
 
+  remover() {
+    document.getElementById(this.id).remove();
+  }
+  adicionar() {
+    const btnedit = document.createElement("button");
+    btnedit.textContent = "editar";
+    const btnremove = document.createElement("button");
+    btnremove.textContent = "remover";
+    let lista = document.getElementById("lista");
+    let viewtarefa = document.createElement("div");
+    viewtarefa.id = this.id;
+    viewtarefa.className = "viewtarefa";
+    let tarefatxt = document.createElement("input");
+    tarefatxt.type = "text";
+    tarefatxt.value = this.nome;
+    tarefatxt.id = this.id_input;
+    tarefatxt.disabled = true;
+    viewtarefa.appendChild(tarefatxt);
+    viewtarefa.appendChild(btnremove);
+    viewtarefa.appendChild(btnedit);
+    lista.appendChild(viewtarefa);
+    btnremove.addEventListener("click", () => {
+      this.remover();
+    });
+    btnedit.addEventListener("click", () => {
+      this.editar();
+    });
+  }
 }
 
-    remover(){
-document.getElementById(this.id).remove();
-    }
-    adicionar(){
-        const btnremove=document.createElement("button");
-        btnremove.textContent="remover"
-        let lista=document.getElementById("lista")
-        let viewtarefa=document.createElement("div")
-        viewtarefa.id=this.id;
-        let tarefatxt=document.createElement("p");
-        tarefatxt.textContent=this.nome;
-        viewtarefa.appendChild(tarefatxt)
-        viewtarefa.appendChild(btnremove);
-        lista.appendChild(viewtarefa)
-        btnremove.addEventListener("click",()=>{
-            this.remover();
-        })
-    }
-}
-
-btn.addEventListener("click",()=>{
-    counter++
-    let nometarefa=document.getElementById("tarefa").value;
-    let tarefa=new Tarefa(nometarefa,counter);
-    tarefa.adicionar();
-})
-
-
-
+btn.addEventListener("click", () => {
+  counterinput++;
+  counter++;
+  let nometarefa = document.getElementById("tarefa").value;
+  let tarefa = new Tarefa(nometarefa, counter, counterinput);
+  tarefa.adicionar();
+});
